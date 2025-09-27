f06fc452adea384de8269453e250f344cc5db5fb
rsayyed591 <rehansayyed591@gmail.com>
2025-09-27 11:41:41 +0530

    Added Axios component and added react router

--- Day 2/test/src/App.jsx
+++ Day 2/test/src/App.jsx
@@ -1,18 +1,32 @@
 import './App.css'
 import Greetings from './components/Greetings'
 import Timer from './components/Timer'
-import UserCard from './components/UserCard'
 // import (component_name) from '(component_path)'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import UserCard from './components/UserCard'
+import Axios from './pages/Axios'
+
+// src\pages\Axios.jsx
+// C:\Users\rehan\OneDrive\Desktop\Mern Bootcamp\Day 2\test\src\pages\Axios.jsx
 
 function App() {
 
   return (
-    <>
-     <h3>REACT</h3>
-     <Greetings name="name_random"/>
-     <Timer />
-     <UserCard />
-    </>
+    <Router>
+      <h3>REACT</h3>
+      <Greetings name="name_random"/>
+      <Link to='/teams'>Our Teams</Link>
+      <Link to='/'>Home</Link>
+      <Link to='/timer'>Timer</Link>
+      <Link to="/user"> Users </Link>
+
+      <Routes>
+        <Route path='/teams' element={ <UserCard />}/>
+        <Route path='/timer' element={ <Timer />}/>
+        <Route path='/user' element= {<Axios />}/>
+      </Routes>
+      <footer>&copy; @2025, rehan</footer>
+    </Router>
   )
 }
 
